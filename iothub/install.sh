46aaa0e01cacd810cfae15a8a786076300d62ad3
DevScienceScope <developer@sciencescope.uk>
2019-02-13 14:32:47 +0000

    Add files via upload

--- iothub/install.sh
+++ iothub/install.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
+#!/bin/bash
+
 # installer.sh will install the necessary packages to get the gifcam up and running with 
 # basic functions
 
 # Install packages
 PACKAGES="python3 python3-pip python3-serial python3-rpi.gpio"
 apt-get update
-apt-get upgrade -y
-apt-get install $PACKAGES -y
+apt-get install $PACKAGES
 
 #pip packages
 pip3 install pyowm
